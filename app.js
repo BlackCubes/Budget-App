deaf48a22f285684d8dc42fcf67ecfc1d985ffbe
BlackCubes <gutierrezelias1991@gmail.com>
2019-12-05 15:47:01 -0800

    Created a return inside the budget controller to be used publicly, and also created inside it a method

--- app.js
+++ app.js
@@ -47,14 +47,36 @@ var budgetController = (function() {
         percentage: -1
     };
 
-});
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        }
+    };
+
+})();
 
 // UI CONTROLLER
 var UIController = (function() {
 
-});
+})();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-});
+})();
