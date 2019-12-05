692afa21de2127f5d9004c412e874db3ac37a0b0
BlackCubes <gutierrezelias1991@gmail.com>
2019-12-05 15:26:21 -0800

    Created an Expense object with properties, and created two prototypes that are instances of the Expense that are properties or methods

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    }
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    }
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
+    }
+
 });
 
 // UI CONTROLLER
