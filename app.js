8fba940bd0cc7ce220e42d2e574c420d94a70632
BlackCubes <gutierrezelias1991@gmail.com>
2019-12-06 15:47:17 -0800

    Created private method that stores the event listeners from the DOM

--- app.js
+++ app.js
@@ -274,4 +274,21 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var setupEventListeners = function() {
+        var DOM = UIController.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
+
+    };
+
 })();
