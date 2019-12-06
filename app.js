2e06d84d949c69987a7d5aa57407bcfb253e7679
BlackCubes <gutierrezelias1991@gmail.com>
2019-12-06 12:38:35 -0800

    Created an object in the UI controller which stores the HTML classes

--- app.js
+++ app.js
@@ -123,6 +123,22 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container',
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
+    };
+
 })();
 
 // GLOBAL APP CONTROLLER
